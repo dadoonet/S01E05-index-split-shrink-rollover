dfe2f63fe3a3548d385a0116ce9f8381a70dc1d2
David Pilato <david@pilato.fr>
2021-03-11 15:21:09 +0100

    Use full URLs instead of forcing Elasticsearch

--- run.sh
+++ run.sh
@@ -15,14 +15,16 @@ check_service () {
 	echo $1 is now up.
 }
 
-# Curl Post call Param 1 is the URL, Param 2 is a json file, Param 3 is optional text
+# Curl Post call Param 1 is the Full URL, Param 2 is a json file, Param 3 is optional text
+# curl_post "$ELASTICSEARCH_URL/demo-rollover-write/_doc" "elasticsearch-config/foo.json" "Fancy text"
+# curl_post "$ELASTICSEARCH_URL/demo-rollover-write/_doc" "elasticsearch-config/foo.json"
 curl_post () {
 	if [ -z "$3" ] ; then
-		echo "Calling POST /$1"
+		echo "Calling POST $1"
 	else 
 	  echo $3
 	fi
-  curl -XPOST "$ELASTICSEARCH_URL/$1" -u elastic:$ELASTIC_PASSWORD -H 'Content-Type: application/json' -d"@$2" ; echo
+  curl -XPOST "$1" -u elastic:$ELASTIC_PASSWORD -H 'Content-Type: application/json' -d"@$2" ; echo
 }
 
 # Start of the script
@@ -50,7 +52,7 @@ echo "Press [CTRL+C] to stop.."
 
 while true
 do
-  curl_post "demo-rollover-write/_doc" "elasticsearch-config/foo.json"
+  curl_post "$ELASTICSEARCH_URL/demo-rollover-write/_doc" "elasticsearch-config/foo.json"
   sleep 1
 done
 
